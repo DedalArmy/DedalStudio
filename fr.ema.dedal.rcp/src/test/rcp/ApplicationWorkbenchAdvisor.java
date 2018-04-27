b847b57f3288f86f8d6aca918be4d9bb98dea4a8
vcolas <vcolas@COLAS.personnel.mines-ales.fr>
2018-04-27 17:09:25 +0200

    2nd commit

--- fr.ema.dedal.rcp/src/test/rcp/ApplicationWorkbenchAdvisor.java
+++ fr.ema.dedal.rcp/src/test/rcp/ApplicationWorkbenchAdvisor.java
@@ -1,60 +1,66 @@
 package test.rcp;
 
-
 import java.net.URL;
+import java.util.Properties;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEInternalWorkbenchImages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 import org.osgi.framework.Bundle;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String PERSPECTIVE_ID = "test.rcp.perspective"; //$NON-NLS-1$
+	private static final String PERSPECTIVE_ID = "test.rcp"; //$NON-NLS-1$
 
 	@Override
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
-    
-    @Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
 	}
-    
-    @Override
-    public void initialize(IWorkbenchConfigurer configurer) {
-        super.initialize(configurer);
-        
-        // inserted: register workbench adapters
-        IDE.registerAdapters();
-     
-        // inserted: register images for rendering explorer view
-        final String ICONS_PATH = "icons/full/";
-        final String PATH_OBJECT = ICONS_PATH + "obj16/";
-        Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
-        declareWorkbenchImage(configurer, ideBundle,
-            IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif", true);
-        declareWorkbenchImage(configurer, ideBundle,
-            IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT + "cprj_obj.gif", true);
-    }
-     
-    private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
-            Bundle ideBundle, String symbolicName, String path, boolean shared) {
-        URL url = ideBundle.getEntry(path);
-        ImageDescriptor desc = ImageDescriptor.createFromURL(url);
-        configurer_p.declareImage(symbolicName, desc, shared);
-    }
-    
-    @Override
-    public IAdaptable getDefaultPageInput() {
-    	return ResourcesPlugin.getWorkspace().getRoot();
-    }
+
+	public void initialize(IWorkbenchConfigurer configurer) {
+	    super.initialize(configurer);
+	    configurer.setSaveAndRestore(false);
+	    PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS, false);
+	    IDE.registerAdapters();
+	    final String ICONS_PATH = "icons/full/";
+	    Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+	    declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT, ICONS_PATH + "obj16/prj_obj.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, ICONS_PATH + "obj16/cprj_obj.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDEInternalWorkbenchImages.IMG_ETOOL_PROBLEM_CATEGORY, ICONS_PATH + "eview16/problems_view.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDEInternalWorkbenchImages.IMG_ETOOL_PROBLEM_CATEGORY, ICONS_PATH + "eview16/problems_view_error.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDEInternalWorkbenchImages.IMG_ETOOL_PROBLEM_CATEGORY, ICONS_PATH + "eview16/problems_view_warning.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDEInternalWorkbenchImages.IMG_OBJS_ERROR_PATH, ICONS_PATH + "obj16/error_tsk.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle, IDEInternalWorkbenchImages.IMG_OBJS_WARNING_PATH, ICONS_PATH + "obj16/warn_tsk.gif", true);
+	    
+	    }
+	
+	
+
+	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p, Bundle ideBundle, String symbolicName,
+			String path, boolean shared) {
+		URL url = ideBundle.getEntry(path);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		configurer_p.declareImage(symbolicName, desc, shared);
+	}
+
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
 }
