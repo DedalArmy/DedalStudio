d03c225bca6ec49ad85dfd64c4406209f6c68540
vcolas <vcolas@COLAS.personnel.mines-ales.fr>
2018-05-18 17:11:09 +0200

    7th commit component inspector dialog edited

--- fr.ema.dedal.rcp/src/test/rcp/action/InspectorDialog.java
+++ fr.ema.dedal.rcp/src/test/rcp/action/InspectorDialog.java
@@ -10,6 +10,8 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
@@ -28,6 +30,7 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -36,220 +39,225 @@ import org.eclipse.swt.widgets.Shell;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
-public class InspectorDialog extends Dialog {
+import fr.ema.dedal.componentinspector.main.Main;
+
+public class InspectorDialog extends TitleAreaDialog {
 
 	private TreeViewer viewer;
-	
+
 	private static final DateFormat dateFormat = DateFormat.getDateInstance();
-	
-    public InspectorDialog(Shell parentShell) {
-        super(parentShell);
-    }
 
-    @Override
-    protected Control createDialogArea(Composite parent) {
-    	
-        Composite container = (Composite) super.createDialogArea(parent);
-        GridLayout layout = new GridLayout(2, false);
-        layout.marginRight = 5;
-        layout.marginLeft = 10;
-        container.setLayout(layout);
-        
-        viewer = new TreeViewer(container, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-        viewer.setContentProvider(new ViewContentProvider());
-        viewer.getTree().setHeaderVisible(true);
+	public InspectorDialog(Shell parentShell) {
+		super(parentShell);
+	}
 
-        TreeViewerColumn mainColumn = new TreeViewerColumn(viewer, SWT.NONE);
-        mainColumn.getColumn().setText("Name");
-        mainColumn.getColumn().setWidth(300);
-        mainColumn.setLabelProvider(
-                new DelegatingStyledCellLabelProvider(
-                        new ViewLabelProvider(createImageDescriptor())));
+	@Override
+	public void create() {
+		super.create();
+		setTitle("Component Inspector");
+		setMessage("Select the folder you want to inspect", IMessageProvider.INFORMATION);
+	}
 
-        TreeViewerColumn modifiedColumn = new TreeViewerColumn(viewer, SWT.NONE);
-        modifiedColumn.getColumn().setText("Last Modified");
-        modifiedColumn.getColumn().setWidth(100);
-        modifiedColumn.getColumn().setAlignment(SWT.RIGHT);
-        modifiedColumn
-                .setLabelProvider(new DelegatingStyledCellLabelProvider(
-                        new FileModifiedLabelProvider(dateFormat)));
+	@Override
+	protected Control createDialogArea(Composite parent) {
 
-        TreeViewerColumn fileSizeColumn = new TreeViewerColumn(viewer, SWT.NONE);
-        fileSizeColumn.getColumn().setText("Size");
-        fileSizeColumn.getColumn().setWidth(100);
-        fileSizeColumn.getColumn().setAlignment(SWT.RIGHT);
-        fileSizeColumn.setLabelProvider(new DelegatingStyledCellLabelProvider(
-                new FileSizeLabelProvider()));
-        
-        viewer.setInput(File.listRoots());
-        
-     
-     
-        Button button = new Button(parent, SWT.PUSH);
-        button.setText("Start component inspector");
-        button.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-            	ISelection selection = viewer.getSelection();
-            	String location = selection.toString();
-            	location = location.substring(1, location.length()-1);
-            	System.out.println(location);
-            	System.out.println(URI.createURI(location, true));        	
-            }
-        });
+		Composite area = (Composite) super.createDialogArea(parent);
+		Composite container = new Composite(area, SWT.NONE);
+		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		GridLayout layout = new GridLayout(1,true);
+		container.setLayout(layout);
 
-        return container;
-    }
-    
-    private ImageDescriptor createImageDescriptor() {
-        Bundle bundle = FrameworkUtil.getBundle(ViewLabelProvider.class);
-        URL url = FileLocator.find(bundle, new Path("icons/folder.png"), null);
-        return ImageDescriptor.createFromURL(url);
-    }
+		viewer = new TreeViewer(container, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.getTree().setHeaderVisible(true);
 
-    class ViewContentProvider implements ITreeContentProvider {
-        public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-        }
+		TreeViewerColumn mainColumn = new TreeViewerColumn(viewer, SWT.NONE);
+		mainColumn.getColumn().setText("Name");
+		mainColumn.getColumn().setWidth(250);
+		mainColumn.setLabelProvider(
+				new DelegatingStyledCellLabelProvider(new ViewLabelProvider(createImageDescriptor())));
 
-        @Override
-        public void dispose() {
-        }
+		TreeViewerColumn modifiedColumn = new TreeViewerColumn(viewer, SWT.NONE);
+		modifiedColumn.getColumn().setText("Last Modified");
+		modifiedColumn.getColumn().setWidth(100);
+		modifiedColumn.getColumn().setAlignment(SWT.RIGHT);
+		modifiedColumn
+				.setLabelProvider(new DelegatingStyledCellLabelProvider(new FileModifiedLabelProvider(dateFormat)));
 
-        @Override
-        public Object[] getElements(Object inputElement) {
-            return (File[]) inputElement;
-        }
+		TreeViewerColumn fileSizeColumn = new TreeViewerColumn(viewer, SWT.NONE);
+		fileSizeColumn.getColumn().setText("Size");
+		fileSizeColumn.getColumn().setWidth(50);
+		fileSizeColumn.getColumn().setAlignment(SWT.RIGHT);
+		fileSizeColumn.setLabelProvider(new DelegatingStyledCellLabelProvider(new FileSizeLabelProvider()));
 
-        @Override
-        public Object[] getChildren(Object parentElement) {
-            File file = (File) parentElement;
-            return file.listFiles();
-        }
+		viewer.setInput(File.listRoots());
 
-        @Override
-        public Object getParent(Object element) {
-            File file = (File) element;
-            return file.getParentFile();
-        }
+		return container;
+	}
 
-        @Override
-        public boolean hasChildren(Object element) {
-            File file = (File) element;
-            if (file.isDirectory()) {
-                return true;
-            }
-            return false;
-        }
+	private ImageDescriptor createImageDescriptor() {
+		Bundle bundle = FrameworkUtil.getBundle(ViewLabelProvider.class);
+		URL url = FileLocator.find(bundle, new Path("icons/folder.png"), null);
+		return ImageDescriptor.createFromURL(url);
+	}
 
-    }
+	class ViewContentProvider implements ITreeContentProvider {
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
 
-    class ViewLabelProvider extends LabelProvider implements IStyledLabelProvider {
+		@Override
+		public void dispose() {
+		}
 
-        private ImageDescriptor directoryImage;
-        private ResourceManager resourceManager;
+		@Override
+		public Object[] getElements(Object inputElement) {
+			return (File[]) inputElement;
+		}
 
-        public ViewLabelProvider(ImageDescriptor directoryImage) {
-            this.directoryImage = directoryImage;
-        }
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			File file = (File) parentElement;
+			return file.listFiles();
+		}
 
-        @Override
-        public StyledString getStyledText(Object element) {
-            if (element instanceof File) {
-                File file = (File) element;
-                StyledString styledString = new StyledString(getFileName(file));
-                String[] files = file.list();
-                if (files != null) {
-                    styledString.append(" ( " + files.length + " ) ", StyledString.COUNTER_STYLER);
-                }
-                return styledString;
-            }
-            return null;
-        }
+		@Override
+		public Object getParent(Object element) {
+			File file = (File) element;
+			return file.getParentFile();
+		}
 
-        @Override
-        public Image getImage(Object element) {
-            if (element instanceof File) {
-                if (((File) element).isDirectory()) {
-                    return getResourceManager().createImage(directoryImage);
-                }
-            }
+		@Override
+		public boolean hasChildren(Object element) {
+			File file = (File) element;
+			if (file.isDirectory()) {
+				return true;
+			}
+			return false;
+		}
 
-            return super.getImage(element);
-        }
+	}
 
-        @Override
-        public void dispose() {
-            // garbage collection system resources
-            if (resourceManager != null) {
-                resourceManager.dispose();
-                resourceManager = null;
-            }
-        }
+	class ViewLabelProvider extends LabelProvider implements IStyledLabelProvider {
 
-        protected ResourceManager getResourceManager() {
-            if (resourceManager == null) {
-                resourceManager = new LocalResourceManager(JFaceResources.getResources());
-            }
-            return resourceManager;
-        }
+		private ImageDescriptor directoryImage;
+		private ResourceManager resourceManager;
 
-        private String getFileName(File file) {
-            String name = file.getName();
-            return name.isEmpty() ? file.getPath() : name;
-        }
-    }
+		public ViewLabelProvider(ImageDescriptor directoryImage) {
+			this.directoryImage = directoryImage;
+		}
 
-    class FileModifiedLabelProvider extends LabelProvider implements IStyledLabelProvider {
+		@Override
+		public StyledString getStyledText(Object element) {
+			if (element instanceof File) {
+				File file = (File) element;
+				StyledString styledString = new StyledString(getFileName(file));
+				String[] files = file.list();
+				if (files != null) {
+					styledString.append(" ( " + files.length + " ) ", StyledString.COUNTER_STYLER);
+				}
+				return styledString;
+			}
+			return null;
+		}
 
-        private DateFormat dateLabelFormat;
+		@Override
+		public Image getImage(Object element) {
+			if (element instanceof File) {
+				if (((File) element).isDirectory()) {
+					return getResourceManager().createImage(directoryImage);
+				}
+			}
 
-        public FileModifiedLabelProvider(DateFormat dateFormat) {
-            dateLabelFormat = dateFormat;
-        }
+			return super.getImage(element);
+		}
 
-        @Override
-        public StyledString getStyledText(Object element) {
-            if (element instanceof File) {
-                File file = (File) element;
-                long lastModified = file.lastModified();
-                return new StyledString(dateLabelFormat.format(new Date(lastModified)));
-            }
-            return null;
-        }
-    }
+		@Override
+		public void dispose() {
+			// garbage collection system resources
+			if (resourceManager != null) {
+				resourceManager.dispose();
+				resourceManager = null;
+			}
+		}
 
-    class FileSizeLabelProvider extends LabelProvider implements IStyledLabelProvider {
+		protected ResourceManager getResourceManager() {
+			if (resourceManager == null) {
+				resourceManager = new LocalResourceManager(JFaceResources.getResources());
+			}
+			return resourceManager;
+		}
 
-        @Override
-        public StyledString getStyledText(Object element) {
-            if (element instanceof File) {
-                File file = (File) element;
-                if (file.isDirectory()) {
-                    // a directory is just a container and has no size
-                    return new StyledString("0");
-                }
-                return new StyledString(String.valueOf(file.length()));
-            }
-            return null;
-        }
-    }
+		private String getFileName(File file) {
+			String name = file.getName();
+			return name.isEmpty() ? file.getPath() : name;
+		}
+	}
 
-    @Focus
-    public void setFocus() {
-        viewer.getControl().setFocus();
-    }
+	class FileModifiedLabelProvider extends LabelProvider implements IStyledLabelProvider {
 
-    // overriding this methods allows you to set the
-    // title of the custom dialog
-    @Override
-    protected void configureShell(Shell newShell) {
-        super.configureShell(newShell);
-        newShell.setText("Component Inspector");
-    }
+		private DateFormat dateLabelFormat;
 
-    @Override
-    protected Point getInitialSize() {
-        return new Point(450, 300);
-    }
+		public FileModifiedLabelProvider(DateFormat dateFormat) {
+			dateLabelFormat = dateFormat;
+		}
+
+		@Override
+		public StyledString getStyledText(Object element) {
+			if (element instanceof File) {
+				File file = (File) element;
+				long lastModified = file.lastModified();
+				return new StyledString(dateLabelFormat.format(new Date(lastModified)));
+			}
+			return null;
+		}
+	}
+
+	class FileSizeLabelProvider extends LabelProvider implements IStyledLabelProvider {
+
+		@Override
+		public StyledString getStyledText(Object element) {
+			if (element instanceof File) {
+				File file = (File) element;
+				if (file.isDirectory()) {
+					// a directory is just a container and has no size
+					return new StyledString("0");
+				}
+				return new StyledString(String.valueOf(file.length()));
+			}
+			return null;
+		}
+	}
+
+	@Focus
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+
+	// overriding this methods allows you to set the
+	// title of the custom dialog
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+	}
+
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+
+	@Override
+	protected void okPressed() {
+		ISelection selection = viewer.getSelection();
+		String location = selection.toString();
+		location = location.substring(1, location.length() - 1);
+
+		if (!location.equals("empty selection")) {
+			location = location.replace("\\", "/");
+			System.out.println(location);
+			System.out.println(URI.createURI(location, true));
+			Main.main(new String[] {location});
+			super.okPressed();
+		}
+	}
 
 }
